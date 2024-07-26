d9574148b114f8ea64321de18ce2ee28505a6503
Kumar Kartikay <kartikaydwivedi55@gmail.com>
2024-07-27 00:49:31 +0530

    feat: css config added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Lets begin</p>
-    </main>
+    <div>
+      <h1 className="h1-bold">QueyNest is coming</h1>
+    </div>
   );
 }
